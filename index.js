698a1fda3beaa0a4f1dfb977441caf7edbe7e231
keenin99 <anakato4@gmail.com>
2021-09-05 22:56:52 +0300

    add js file

---
+++ index.js
@@ -0,0 +1,8 @@
+//declare variable
+const btn=document.querySelector('#color-change');
+//add event Listener
+btn.addEventListener('click',()=>{
+document.body.style.background="radial-gradient(#FF4848, #FFD371, #9DDAC6)";
+console.log('Success!');
+
+});
